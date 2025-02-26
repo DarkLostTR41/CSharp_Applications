b8cbb5a4f0a8b62654a78ab7279c8b4499b0c5cd
Fatih Aksoy <mfaksoy67@outlook.com>
2025-02-26 14:45:10 +0300

    Add files via upload

---
+++ RainbowConsoleApp/RainbowConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RainbowConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(" GÖKKUŞAĞI RENKLERİ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" SARI              ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" YEŞİL             ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" KIRMIZI           ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" MAVİ              ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(" MOR               ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" LACİVERT          ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" TURUNCU           ");
+            Console.ReadKey();
+        }
+    }
+}
